e4cb259dbf0bd22aa8a7323bd8034f0cce3a4cd1
Gunisetti Gokul <167660982+gokulcoder7@users.noreply.github.com>
2025-06-05 13:45:24 +0530

    Create String_functions_java8.java

---
+++ String_functions_java8.java
@@ -0,0 +1,23 @@
+
+
+        System.out.println(st.indexOf('a')); // -1 if not found
+//st.equals();
+
+        System.out.println(st.compareTo("gokul"));   //0 if same ,   -2 for gok(as ul is missing) , -3 for go
+
+//st.isEmpty()
+
+
+
+System.out.println( st.lastIndexOf('g') );
+
+
+//st.concat()
+
+
+
+//st.replace()   old char with new char
+
+//        st.toCharArray()
+                
+
