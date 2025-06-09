65e7e6ae49d5e65f12e6736bdafeeddbbecb94e4
Gunisetti Gokul <167660982+gokulcoder7@users.noreply.github.com>
2025-06-09 22:16:32 +0530

    Update spanofstock.java
    Input: arr[] = [100, 80, 60, 70, 60, 75, 85]
    Output: [1, 1, 1, 2, 1, 4, 6]
    Explanation: Traversing the given input span 100 is greater than equal to 100 and there are no more elements behind it so the span is 1, 80 is greater than equal to 80 and smaller than 100 so the span is 1, 60 is greater than equal to 60 and smaller than 80 so the span is 1, 70 is greater than equal to 60,70 and smaller than 80 so the span is 2 and so on. Hence the output will be 1 1 1 2 1 4 6.
    Input: arr[] = [10, 4, 5, 90, 120, 80]
    Output: [1, 1, 2, 4, 5, 1]
    Explanation: Traversing the given input span 10 is greater than equal to 10 and there are no more elements behind it so the span is 1, 4 is greater than equal to 4 and smaller than 10 so the span is 1, 5 is greater than equal to 4,5 and smaller than 10 so the span is 2, and so on. Hence the output will be 1 1 2 4 5 1.

--- spanofstock.java
+++ spanofstock.java
@@ -4,6 +4,21 @@ import java.util.*;
 // 7
 // 100 80 60 70 60 75 85
 
+
+// Input: arr[] = [100, 80, 60, 70, 60, 75, 85]
+// Output: [1, 1, 1, 2, 1, 4, 6]
+// Explanation: Traversing the given input span 100 is greater than equal to 100 and there are no more elements behind it so the span is 1, 80 is greater than equal to 80 and smaller than 100 so the span is 1, 60 is greater than equal to 60 and smaller than 80 so the span is 1, 70 is greater than equal to 60,70 and smaller than 80 so the span is 2 and so on. Hence the output will be 1 1 1 2 1 4 6.
+// Input: arr[] = [10, 4, 5, 90, 120, 80]
+// Output: [1, 1, 2, 4, 5, 1]
+// Explanation: Traversing the given input span 10 is greater than equal to 10 and there are no more elements behind it so the span is 1, 4 is greater than equal to 4 and smaller than 10 so the span is 1, 5 is greater than equal to 4,5 and smaller than 10 so the span is 2, and so on. Hence the output will be 1 1 2 4 5 1.
+
+
+
+
+
+
+
+
 public class new2 {
     public static void main(String[] args) {
 
